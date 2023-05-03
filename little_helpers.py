0c8eef2f483161d495d9acaa4608c80515b1ee22
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-05-03 18:56:23 +0000

    helper functions

---
+++ little_helpers.py
@@ -0,0 +1,22 @@
+import os
+import json
+import pandas as pd
+from PIL import Image
+from IPython.display import display, HTML
+import base64
+
+
+# from AOKVQA git (https://github.com/allenai/aokvqa#downloading-the-dataset)
+
+def load_aokvqa(aokvqa_dir, split, version='v1p0'):
+    assert split in ['train', 'val', 'test', 'test_w_ans']
+    dataset = json.load(open(
+        os.path.join(aokvqa_dir, f"aokvqa_{version}_{split}.json")
+    ))
+    return dataset
+
+
+# from AOKVQA git (https://github.com/allenai/aokvqa#downloading-the-dataset)
+
+def get_coco_path(split, image_id, coco_dir):
+    return os.path.join(coco_dir, f"{split}", f"{image_id:012}.jpg")
