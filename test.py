e58f8e9180da3e983a1d6371c46e487f4dbb0287
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-06-01 15:17:31 +0000

    test

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
-cp -R datasets/coco2017/train/* datasets/coco2017/val/* datasets/coco2017/test/* datasets/coco2017/all/
-
-
-datasets/coco2017/train
-datasets/coco2017/val
-datasets/coco2017/test
-datasets/coco2017/all
+test
