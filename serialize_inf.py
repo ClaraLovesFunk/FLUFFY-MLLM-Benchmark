26d12346193143a98f1b6675f5d6c3b21ca53fbe
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-06-14 19:48:39 +0000

    clean serialization script

--- serialize_inf.py
+++ serialize_inf.py
@@ -23,6 +23,7 @@ def get_model(model_name, device):
     lavis_name = model_info.get_lavis_name()
 
     model, vis_processors, _ = load_model_and_preprocess(name = lavis_name, model_type = lavis_model_type, is_eval = True, device = device)
+    
     print('model loaded')
 
     return model, vis_processors
@@ -34,9 +35,7 @@ def get_dataset_text(dataset_name):
 
 
     dataset_info = DatasetInfo(dataset_name)
-
     text_dataset_split = dataset_info.get_text_dataset_split()
-
     dataset_file_path = os.path.join('datasets', dataset_name, text_dataset_split + '.json')
     
     data_text = dataset(dataset_name, dataset_file_path).load()
@@ -48,18 +47,15 @@ def get_dataset_text(dataset_name):
 
 
 
-def gen_output(device, dataset_name, data_text, model, vis_processors, prep_image, prompt_construct):
-
+def gen_output(device, dataset_name, data_text, model, vis_processors, prompt_construct):
 
 
     dataset_info = DatasetInfo(dataset_name)
-
     img_dataset_split = dataset_info.get_img_dataset_split() # find split on which we can eval model (ideally test, otherwise val)
     image_dataset_name = dataset_info.get_img_dataset() # find corresponding image dataset
     tasks = dataset_info.get_tasks() # find tasks associated with current dataset
 
-    
-
+ 
     pred = [] 
 
     for t in tasks:
@@ -68,11 +64,11 @@ def gen_output(device, dataset_name, data_text, model, vis_processors, prep_imag
 
         for sample in data_text:
 
-            #image = prep_image(device, images_dir_path, sample, vis_processors)
             # prep image
-            images_dir_path = os.path.join('datasets', image_dataset_name, img_dataset_split)
 
+            images_dir_path = os.path.join('datasets', image_dataset_name, img_dataset_split)
             image_file_path =  os.path.join(images_dir_path, f"{sample['image_id']:012}.jpg")
+            
             image_raw = Image.open(image_file_path) 
             
             if image_raw.mode != 'RGB': 
@@ -81,14 +77,17 @@ def gen_output(device, dataset_name, data_text, model, vis_processors, prep_imag
             image = vis_processors["eval"](image_raw).unsqueeze(0).to(device)
 
             # make prompt
+
             prompt = prompt_construct(test_sample = sample,task = t)
 
             # generate output
+
             output = model.generate({"image": image, "prompt": prompt})
 
             sample.update({output_task: output})
             pred.append(sample)
 
+            
         print(f'output generated for task "{t}"')
 
     return pred
@@ -112,7 +111,12 @@ def save_output(pred, model_name, dataset_name, run, check_create_experiment_dir
 
 
 
-##################################################################
+###########################################################################################
+###########################################################################################
+################################   RUN THE DAMN THING   ###################################
+###########################################################################################
+###########################################################################################
+
 
 model_name = ['blip2']
 dataset_name = ['okvqa', 'aokvqa']
@@ -130,7 +134,7 @@ for m in model_name:
 
         for r in run:
 
-            pred = gen_output(device = device, dataset_name = ds, data_text = data_text, model = model, vis_processors = vis_processors, prep_image = prep_image, prompt_construct = prompt_construct)
+            pred = gen_output(device = device, dataset_name = ds, data_text = data_text, model = model, vis_processors = vis_processors, prompt_construct = prompt_construct)
             save_output(pred = pred, model_name = m, dataset_name = ds, run = r, check_create_experiment_dir = check_create_experiment_dir)
             
             print('\n')
