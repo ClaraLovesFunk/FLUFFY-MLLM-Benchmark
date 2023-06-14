f6ace79d58a551b3f72d7e0425b3f5c1f24cb2ba
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-06-14 15:06:59 +0000

    serialize inference - lets see if works

--- serialize_inf_module.py
+++ serialize_inf_module.py
@@ -15,16 +15,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 
-#m = 'blip2'
-#ds = 'aokvqa'
-#r = 1
-
-
-
-#def inference(model_name, dataset_name, run, device, prep_image, prompt_construct):
-
-
-def get_model():
+def get_model(model_name):
 
 
     model_info = ModelInfo(model_name)
@@ -39,18 +30,15 @@ def get_model():
 
 
 
-def get_dataset_text():
+def get_dataset_text(dataset_name):
 
 
     dataset_info = DatasetInfo(dataset_name)
 
     split = dataset_info.get_split()
-    images = dataset_info.get_img_dataset()
-    tasks = dataset_info.get_tasks()
 
     dataset_file_path = os.path.join('datasets', dataset_name, split + '.json')
-    images_dir_path = os.path.join('datasets', images, split)
-
+    
     data_text = dataset(dataset_name, dataset_file_path).load()
 
     print('dataset loaded')
@@ -59,40 +47,40 @@ def get_dataset_text():
 
 
 
-# paths
+
+def gen_output(device, dataset_name, model, vis_processors, prep_image, prompt_construct):
+
+
+    dataset_info = DatasetInfo(dataset_name)
+
+    split = dataset_info.get_split() # find split on which we can eval model (ideally test, otherwise val)
+    images = dataset_info.get_img_dataset() # find corresponding image dataset
+    tasks = dataset_info.get_tasks() # find tasks associated with current dataset
+
+    images_dir_path = os.path.join('datasets', images, split)
+
+    pred = [] 
+
+    for sample in data_text:
+
+        image = prep_image(device, images_dir_path, sample, vis_processors)
+
+        for t in tasks:
+
+            prompt = prompt_construct(test_sample = sample,task = t)
+            output = model.generate({"image": image, "prompt": prompt})
+
+            output_task = 'output_' + t
+            sample.update({output_task: output})
+            pred.append(sample)
+
+    print('output generated')
+
+    return pred
 
 
 
-
-
-# dataset
-
-
-
-
-
-# output generation 
-
-pred = [] 
-
-for sample in data_text:
-
-    image = prep_image(device, images_dir_path, sample, vis_processors)
-
-    for t in tasks:
-
-        prompt = prompt_construct(test_sample = sample,task = t)
-        output = model.generate({"image": image, "prompt": prompt})
-
-        output_task = 'output_' + t
-        sample.update({output_task: output})
-        pred.append(sample)
-
-
-print('input processed')
-
-
-def save_output():
+def save_output(pred, model_name, dataset_name, run, check_create_experiment_dir):
 
     experiment_dir_path = os.path.join('experiments', model_name, dataset_name, 'run' + str(run))
     experiment_output_file_path = os.path.join(experiment_dir_path, 'output.json')
@@ -101,6 +89,8 @@ def save_output():
 
     with open(experiment_output_file_path, 'w') as f: 
         json.dump(pred,f)
+
+    print('save output')
 
 
 
@@ -117,8 +107,21 @@ run = [1]
 
 for m in model_name:
 
-for ds in dataset_name:
+    model, vis_processors = get_model(m)
 
-    for r in run:
+    for ds in dataset_name:
 
-        inference(model_name = m, dataset_name = ds, run = r, device = device, prep_image = prep_image, prompt_construct = prompt_construct)
+        data_text = get_dataset_text(ds)
+
+        for r in run:
+
+            print('about to run pred')
+            pred = gen_output(device = device, dataset_name = ds, model = model, vis_processors = vis_processors, prep_image = prep_image, prompt_construct = prompt_construct)
+            print('just ran pred')
+            save_output(pred = pred, model_name = m, dataset_name = ds, run = r, check_create_experiment_dir = check_create_experiment_dir)
+            
+            print('\n')
+            print('\n')
+            print('\n')
+            print('YEAAAAAAHHHHHHHHHHHH')
+       
