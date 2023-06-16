a5079f80ffbc0336ce48dabac2fdd3486de6974f
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-06-16 15:26:46 +0000

    updated eval_aokvqa.py

--- eval_aokvqa.py
+++ eval_aokvqa.py
@@ -8,7 +8,7 @@ model_sec = 'blip2'
 
 images_input_dir = 'datasets/' + images_sec
 text_input_dir = 'datasets/' + dataset_sec +'/'
-results_dir = 'experiments/' + model_sec + '/' + dataset_sec + '/'
+results_dir = 'experiments/' + model_sec + '/' + dataset_sec + '/' + 'run1/' 
 
 text_input_file = text_input_dir + split_sec + '.json'
 text_output_file = results_dir + 'output.json'
@@ -25,47 +25,6 @@ with open(text_input_file, 'r') as f:
     input = json.load(f)
 
 
+acc_aokvqa_da = eval_aokvqa(input = input, output=output, task = 'direct_answer', strict=True)
+acc_aokvqa_MC = eval_aokvqa(input = input, output=output, task = 'multiple_choice', strict=True)
 
-# task "direct answer": eval + get example indice
-
-acc_strict_standard_da, example_indice_da = acc_strict_standard(input = input, output = output, multiple_choice=False, strict=True)
-acc_aokvqa_da = eval_aokvqa(input = input, output=output, multiple_choice=False, strict=True)
-
-scores_da = {"acc": acc_aokvqa_da} #"acc_strict_standard": acc_strict_standard_da,}
-
-
-
-# task "Multiple Choice": eval + get example indice
-
-acc_strict_standard_MC, example_indice_MC = acc_strict_standard(input = input, output = output, multiple_choice=True, strict=True)
-acc_aokvqa_MC = eval_aokvqa(input = input, output=output, multiple_choice=True, strict=True)
-
-scores_MC = {"acc": acc_aokvqa_MC} #{"acc_strict_standard": acc_strict_standard_MC,
-          
-
-
-
-# store metrics + example indice
-
-scores_alltasks = {"direct answer": scores_da,
-                   "multiple choice": scores_MC}
-
-example_indice_alltasks = {"direct answer": example_indice_da,
-                           "multiple choice": example_indice_MC}
-
-
-print(scores_alltasks)
-
-with open(eval_file, 'w') as f: 
-    json.dump(scores_alltasks,f)
-
-with open(example_file, 'w') as f:
-    json.dump(example_indice_alltasks,f)
-
-
-
-
-
-
-
-# %%
