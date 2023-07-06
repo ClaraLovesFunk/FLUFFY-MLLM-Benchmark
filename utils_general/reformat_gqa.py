1719d71f47077a5c5fff2f302041ee20f5ba772a
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-07-06 16:04:17 +0000

    add readme style info on what gqa ds to use

---
+++ utils_general/reformat_gqa.py
@@ -0,0 +1,9 @@
+'''
+Using val, because test has no lables.
+Using balanced dataset, because the dataset is gigantic (22 Mio) and 
+balancedness had the following advantage:
+
+"We also made sure to balance the dataset, tightly controlling the answer 
+distribution for different groups of questions, in order to prevent educated 
+guesses using language and world priors." (https://cs.stanford.edu/people/dorarad/gqa/about.html)
+'''
