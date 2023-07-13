f394e2ddc8fd01a14781b6ad381c0bc70645ce22
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-07-13 07:25:43 +0000

    delete unnessecary eval scikit scores

--- eval.py
+++ eval.py
@@ -20,7 +20,7 @@ examples_file_name = 'examples.json' # file indicating which sample was predicte
 # experiment variables
 
 model_name = ['blip2']
-dataset_name = ['hateful_memes']  # 'okvqa','aokvqa', 'mvsa', 'mami', 'hateful_memes'
+dataset_name = ['clevr', 'gqa', 'esnlive']  # 'okvqa','aokvqa', 'mvsa', 'mami', 'hateful_memes'
 run = [1]
 
 
@@ -118,10 +118,7 @@ for m in model_name:
                     'accuracy': metrics.accuracy_score(y_true, y_pred),
                     'precision': metrics.precision_score(y_true, y_pred),
                     'recall': metrics.recall_score(y_true, y_pred),
-                    'f1': metrics.f1_score(y_true, y_pred),
-                    #'roc_auc': metrics.roc_auc_score(y_true, y_pred),
-                    #'log_loss': metrics.log_loss(y_true, y_pred),
-                    #'jaccard_score': metrics.jaccard_score(y_true, y_pred)                    
+                    'f1': metrics.f1_score(y_true, y_pred),                  
                 }
 
                 
@@ -166,18 +163,7 @@ for m in model_name:
                     'accuracy': metrics.accuracy_score(y_true, y_pred),
                     'precision (weighted)': metrics.precision_score(y_true, y_pred, average='weighted'),
                     'recall (weighted)': metrics.recall_score(y_true, y_pred, average='weighted'),
-                    'f1 (weighted)': metrics.f1_score(y_true, y_pred, average='weighted'), ####### ADD OTHER AVERAGING METHODS
-                    #'roc_auc (weighted)': metrics.roc_auc_score(y_true, y_pred, average='weighted', multi_class='ovr'),
-                    #'log_loss': metrics.log_loss(y_true, y_pred),
-                    #'jaccard_score (weighted)': metrics.jaccard_score(y_true, y_pred, average='weighted'),
-                    #'zero_one_loss': metrics.zero_one_loss(y_true, y_pred),
-                    #'balanced_accuracy': metrics.balanced_accuracy_score(y_true, y_pred),
-                    #'cohen_kappa': metrics.cohen_kappa_score(y_true, y_pred),
-                    #'hamming_loss': metrics.hamming_loss(y_true, y_pred),
-                    #'classification_report': metrics.classification_report(y_true, y_pred),
-                    #'confusion_matrix': metrics.confusion_matrix(y_true, y_pred),
-                    #'normalized_mutual_info_score': metrics.normalized_mutual_info_score(y_true, y_pred),
-                    #'adjusted_rand_score': metrics.adjusted_rand_score(y_true, y_pred)
+                    'f1 (weighted)': metrics.f1_score(y_true, y_pred, average='weighted')
                 }
 
                 for input_i in data_text:
@@ -222,18 +208,7 @@ for m in model_name:
                     'accuracy': metrics.accuracy_score(y_true, y_pred),
                     'precision (weighted)': metrics.precision_score(y_true, y_pred, average='weighted'),
                     'recall (weighted)': metrics.recall_score(y_true, y_pred, average='weighted'),
-                    'f1 (weighted)': metrics.f1_score(y_true, y_pred, average='weighted'), ####### ADD OTHER AVERAGING METHODS
-                    #'roc_auc (weighted)': metrics.roc_auc_score(y_true, y_pred, average='weighted', multi_class='ovr'),
-                    #'log_loss': metrics.log_loss(y_true, y_pred),
-                    #'jaccard_score (weighted)': metrics.jaccard_score(y_true, y_pred, average='weighted'),
-                    #'zero_one_loss': metrics.zero_one_loss(y_true, y_pred),
-                    #'balanced_accuracy': metrics.balanced_accuracy_score(y_true, y_pred),
-                    #'cohen_kappa': metrics.cohen_kappa_score(y_true, y_pred),
-                    #'hamming_loss': metrics.hamming_loss(y_true, y_pred),
-                    #'classification_report': metrics.classification_report(y_true, y_pred),
-                    #'confusion_matrix': metrics.confusion_matrix(y_true, y_pred),
-                    #'normalized_mutual_info_score': metrics.normalized_mutual_info_score(y_true, y_pred),
-                    #'adjusted_rand_score': metrics.adjusted_rand_score(y_true, y_pred)
+                    'f1 (weighted)': metrics.f1_score(y_true, y_pred, average='weighted')
                 }
 
                 
