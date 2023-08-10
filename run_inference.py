651ee6d462ca4bd2335ab34fba447b5d8b019135
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-08-10 11:25:10 +0000

    smallest llava temperature: 0.001

--- run_inference.py
+++ run_inference.py
@@ -9,16 +9,15 @@ import sys
 import argparse
 import subprocess
 
-def run_inference(model, dataset):
-    # Base directories for models and datasets
+def run_inference(model_name, dataset_name):
+    
     base_model_dir = "/home/users/cwicharz/project/Testing-Multimodal-LLMs/models"
     base_dataset_dir = "/home/users/cwicharz/project/Testing-Multimodal-LLMs/datasets"
-    venv_base_dir = "/home/users/cwicharz/project/Testing-Multimodal-LLMs/venvs" # Assuming virtual environments are in this directory
+    venv_base_dir = "/home/users/cwicharz/project/Testing-Multimodal-LLMs/venvs" 
 
-    # Construct full paths
-    model_path = os.path.join(base_model_dir, model)
-    dataset_path = os.path.join(base_dataset_dir, dataset, "dev.json")
-    venv_path = os.path.join(venv_base_dir, model, "bin", "python") # Using the Python interpreter directly
+    model_path = os.path.join(base_model_dir, model_name)
+    dataset_path = os.path.join(base_dataset_dir, dataset_name, "dev.json")
+    venv_path = os.path.join(venv_base_dir, model_name, "bin", "python") # Using the Python interpreter directly
 
     # Ensure model, dataset directories/files and virtual environment exist
     if not os.path.exists(model_path):
@@ -30,20 +29,24 @@ def run_inference(model, dataset):
         return
 
     if not os.path.exists(venv_path):
-        print(f"Virtual environment for {model} does not exist!")
+        print(f"Virtual environment for {model_name} does not exist!")
         return
 
     # Run inference script using the Python interpreter from the virtual environment
-    cmd = [venv_path, os.path.join(model_path, "inference.py"), "--dataset", dataset_path]
+    cmd = [venv_path, os.path.join(model_path, "inference.py"), "--dataset", dataset_name]
     env = os.environ.copy()  # Get the current environment variables.
     env["TRANSFORMERS_CACHE"] = CACHE_DIR
     subprocess.run(cmd, env=env)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run inference on a model with a given dataset.")
-    parser.add_argument("-model", type=str, required=True, help="Name of the model directory.")
-    parser.add_argument("-dataset", type=str, required=True, help="Name of the dataset directory.")
+    parser.add_argument("-model", type=str, required=True, help="Name of the model.")
+    parser.add_argument("-dataset", type=str, required=True, help="Name of the dataset.")
     
     args = parser.parse_args()
 
     run_inference(args.model, args.dataset)
+
+
+
+# python3 run_inference.py -model llava -dataset hateful_memes
