08fd2d3bc47b63a57b3ff0adef9a3cb085bcef46
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-09-09 17:29:50 +0000

    prompt-eng: add constraints

--- utils_llava.py
+++ utils_llava.py
@@ -43,39 +43,90 @@ def prompt_construct(test_sample, task):
 
      
 
-    if task == 'direct answer': 
+
+    if task == 'direct answer (aokvqa)': 
         text_input = test_sample['text_input']
-        instruction = 'Answer the following question! '
+        instruction = '''
+                        Answer the following question adhering to these guidelines:
+                        1. Omit articles (like 'a', 'an', 'the') before nouns.
+                        2. Represent all numbers in word form, not as digits.
+                        '''
         prompt =  instruction +  '\n' + question_formal +  text_input +  '\n' + answer_formal
 
-    if task == 'multiple choice': 
+    if task == 'direct answer (okvqa)': 
         text_input = test_sample['text_input']
-        instruction = 'Answer the following question by selecting from the choices below! '
+        instruction = '''
+                        Answer the following question adhering to these guidelines:
+                        1. Omit articles (like 'a', 'an', 'the') before nouns.
+                        2. Represent all numbers as digits, not word form.
+                        '''
+        prompt =  instruction +  '\n' + question_formal +  text_input +  '\n' + answer_formal
+
+    if task == 'direct answer (clevr)': 
+        text_input = test_sample['text_input']
+        instruction = '''
+                        Answer the following question adhering to these guidelines:
+                        1. Omit articles (like 'a', 'an', 'the') before nouns.
+                        2. Represent all numbers as digits, not word form.
+                        '''
+        prompt =  instruction +  '\n' + question_formal +  text_input +  '\n' + answer_formal
+
+
+    if task == 'direct answer (gqa)': 
+        text_input = test_sample['text_input']
+        instruction = '''
+                        Answer the following question adhering to these guidelines:
+                        1. Omit articles (like 'a', 'an', 'the') before nouns.
+                        2. Represent all numbers as digits, not word form.
+                        '''
+        prompt =  instruction +  '\n' + question_formal +  text_input +  '\n' + answer_formal
+
+
+
+    if task == 'multiple choice (aokvqa)': 
+        text_input = test_sample['text_input']
+        instruction = '''
+                        Answer the question by choosing the correct index from the options below. 
+                        For the first answer, write '1'; for the second, write '2', and so on.
+                        '''
         choices_content = test_sample['answer_choices']
         choices_content = ', '.join(choices_content)
         prompt =  instruction +  '\n' + question_formal +  text_input + '\n' + choices_formal  + choices_content + choices_end_formal +  '\n' + answer_formal
     
     if task == 'multiple choice (sqa)': 
         text_input = test_sample['text_input']
-        instruction = 'Answer the following question by giving the zero-based index of the selected answer. If you want to select the first answer, write "0", if you want to select the second answer, write "1" and so on.'
+        instruction = '''
+                        Answer the question by choosing the correct index from the options below. 
+                        For the first answer, write '0'; for the second, write '1', and so on.
+                        '''
         choices_content = test_sample['answer_choices']
         choices_content = ', '.join(choices_content)
         prompt =  instruction +  '\n' + question_formal +  text_input + '\n' + choices_formal  + choices_content + choices_end_formal +  '\n' + answer_formal
     
     if task == 'sentiment analysis':
         text_input = test_sample['text_input']
-        instruction = 'Predict the sentiment of the tweet in combination with the image! The sentiment can be either "Positive", "Negative" or "Neutral".'
+        instruction = '''
+                        Predict the sentiment of the tweet in combination with the image! 
+                        The sentiment can be either "Positive", "Negative" or "Neutral". 
+                        Respond only with one of these three options.
+                        '''
         prompt =  instruction +  '\n' +  tweet_formal + text_input + '\n' + sentiment_formal
 
     if task == 'sexism classification':
         text_input = test_sample['text_input']
-        instruction = "Classify the following meme as 'sexist' or 'not-sexist'."
+        instruction = '''
+                        Classify the following meme as 'sexist' or 'not sexist'. 
+                        Respond only with one of these two options.
+                        '''
         prompt =  instruction +  '\n' +  meme_text_formal + text_input + '\n' + sexist_label_formal
 
     if task == 'hate classification':
         
         text_input = test_sample['text_input']
-        instruction = "Classify the meme as either 'hateful' or 'not-hateful'. Respond only with one of these two options."
+        instruction = '''
+                        Classify the meme as either 'hateful' or 'not hateful'. 
+                        Respond only with one of these two options.
+                        '''
         prompt =  instruction +  '\n' +  meme_text_formal + text_input + '\n' + hate_label_formal
 
     if task == 'entailment prediction':
@@ -83,7 +134,9 @@ def prompt_construct(test_sample, task):
         instruction = '''
                         Classify the following image as 'entailment', if there is enough evidence in the image to conclude that the following hypothesis is true. 
                         Classify the following image as 'contradiction', if there is enough evidence in the image to conclude that the following hypothesis is false.
-                        Classify the following image as 'neutral', if neither of the earlier two are true.'''
+                        Classify the following image as 'neutral', if neither of the earlier two are true.
+                        Respond only with one of these three options.
+                        '''
         prompt =  instruction +  '\n' +  hypothesis_text_formal + text_input + '\n' + entailment_label
 
 
@@ -135,14 +188,14 @@ class DatasetInfo():
         }
 
         self.tasks = {
-            'aokvqa': ['direct answer', 'multiple choice'],  #### 
-            'okvqa': ['direct answer'],
+            'aokvqa': ['direct answer (aokvqa)', 'multiple choice (aokvqa)'], 
+            'okvqa': ['direct answer (okvqa)'],
             'mvsa': ['sentiment analysis'],
             'mami': ['sexism classification'],
             'hateful_memes': ['hate classification'],
-            'clevr': ['direct answer'],
+            'clevr': ['direct answer (clevr)'],
             'esnlive': ['entailment prediction'],
-            'gqa': ['direct answer'],
+            'gqa': ['direct answer (gqa)'],
             'scienceqa': ['multiple choice (sqa)']                                        
         }
 
