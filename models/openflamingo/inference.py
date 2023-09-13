9a22e0bb80536bd8deac4f71ca7242593679ac53
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-09-13 18:07:29 +0000

    clean openflamingo inf on sample

--- models/openflamingo/inference.py
+++ models/openflamingo/inference.py
@@ -13,12 +13,6 @@ import time
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-
-
-
-
-
 start_time= time.time() ########
 
 model, image_processor, tokenizer = create_model_and_transforms(
@@ -33,104 +27,11 @@ checkpoint_path = hf_hub_download("openflamingo/OpenFlamingo-9B-vitl-mpt7b", "ch
 model.load_state_dict(torch.load(checkpoint_path), strict=False)
 model.to(device)
 
-
-
-demo_image_one = Image.open(
-    requests.get(
-        "http://images.cocodataset.org/val2017/000000039769.jpg", stream=True
-    ).raw
-)
-
-demo_image_two = Image.open(
-    requests.get(
-        "http://images.cocodataset.org/test-stuff2017/000000028137.jpg",
-        stream=True
-    ).raw
-)
-
-query_image = Image.open(
-    requests.get(
-        "http://images.cocodataset.org/test-stuff2017/000000028352.jpg", 
-        stream=True
-    ).raw
-)
-
-
-
-# demo_image_one = Image.open('datasets/hateful_memes/images/all/98764.png')
-
-# demo_image_two = Image.open('datasets/hateful_memes/images/all/98762.png')
-
-# query_image = Image.open('datasets/hateful_memes/images/all/98756.png')
-
-
-
-"""
-Step 2: Preprocessing images
-Details: For OpenFlamingo, we expect the image to be a torch tensor of shape 
- batch_size x num_media x num_frames x channels x height x width. 
- In this case batch_size = 1, num_media = 3, num_frames = 1,
- channels = 3, height = 224, width = 224.
-"""
-vision_x = [image_processor(demo_image_one).unsqueeze(0).to(device), image_processor(demo_image_two).unsqueeze(0).to(device), image_processor(query_image).unsqueeze(0).to(device)]
-vision_x = torch.cat(vision_x, dim=0)
-vision_x = vision_x.unsqueeze(1).unsqueeze(0)
-
-
-
-######      EXPERIMENT 1
-
-
-"""
-In the text we expect an <image> special token to indicate where an image is.
- We also expect an <|endofchunk|> special token to indicate the end of the text 
- portion associated with an image.
-"""
-
-tokenizer.padding_side = "left" # For generation padding tokens should be on the left
-
-lang_x = tokenizer(
-    ["<image>An image of two cats.<|endofchunk|><image>An image of a bathroom sink.<|endofchunk|><image>An image of"],
-    return_tensors="pt",
-)
-lang_x.to(device)
-
-
-generated_text = model.generate(
-    vision_x=vision_x,
-    lang_x=lang_x["input_ids"],
-    attention_mask=lang_x["attention_mask"],
-    max_new_tokens=20,
-    num_beams=3,
-    #pad_token_id=model.config.eos_token_id ####### new to avoid depricated version
-)
-
-print('\n')
-print('\n')
-print('\n')
-
-print('Experiment 1: normal demo')
-print("Generated text: ", tokenizer.decode(generated_text[0]))
-
-
-
-
-
-
-######      EXPERIMENT 2
-
-
-
-
-
 demo_image_one = Image.open('datasets/hateful_memes/images/all/98764.png')
-
 demo_image_two = Image.open('datasets/hateful_memes/images/all/98762.png')
-
 query_image = Image.open('datasets/hateful_memes/images/all/98756.png')
 
 
-
 """
 Step 2: Preprocessing images
 Details: For OpenFlamingo, we expect the image to be a torch tensor of shape 
@@ -138,6 +39,7 @@ Details: For OpenFlamingo, we expect the image to be a torch tensor of shape
  In this case batch_size = 1, num_media = 3, num_frames = 1,
  channels = 3, height = 224, width = 224.
 """
+
 vision_x = [image_processor(demo_image_one).unsqueeze(0).to(device), image_processor(demo_image_two).unsqueeze(0).to(device), image_processor(query_image).unsqueeze(0).to(device)]
 vision_x = torch.cat(vision_x, dim=0)
 vision_x = vision_x.unsqueeze(1).unsqueeze(0)
@@ -150,7 +52,7 @@ In the text we expect an <image> special token to indicate where an image is.
  portion associated with an image.
 """
 
-tokenizer.padding_side = "left" # For generation padding tokens should be on the left
+tokenizer.padding_side = "left" 
 
 lang_x = tokenizer(
     ["<image>An image of two cats.<|endofchunk|><image>An image of a bathroom sink.<|endofchunk|><image>An image of"],
@@ -165,24 +67,9 @@ generated_text = model.generate(
     attention_mask=lang_x["attention_mask"],
     max_new_tokens=20,
     num_beams=3,
-    #pad_token_id=model.config.eos_token_id ####### new to avoid depricated version
 )
 
-print('\n')
-print('\n')
-print('\n')
-
-print('Experiment 1: normal demo')
 print("Generated text: ", tokenizer.decode(generated_text[0]))
-
-
-
-
-
-
-
-
-
 
 
 
@@ -193,8 +80,6 @@ end_time= time.time()
 run_time = end_time - start_time
 print(f'runtime: {run_time}')
 
-
 # source venvs/openflamingo/bin/activate
 # cd models/openflamingo
 # python inference.py --dataset hateful_memes
-
