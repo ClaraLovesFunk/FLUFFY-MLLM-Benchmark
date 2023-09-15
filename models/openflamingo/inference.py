3dfe00b2427f31ad12c0dc8aebb16ee0ad8c52ba
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-09-15 14:19:55 +0000

    openflamingo zeroshot: working prompt & output

--- models/openflamingo/inference.py
+++ models/openflamingo/inference.py
@@ -26,8 +26,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 
-def eval_model(prompt, image_file):
-
+def load_model():
     model, image_processor, tokenizer = create_model_and_transforms(
         clip_vision_encoder_path="ViT-L-14",
         clip_vision_encoder_pretrained="openai",
@@ -39,7 +38,10 @@ def eval_model(prompt, image_file):
     checkpoint_path = hf_hub_download("openflamingo/OpenFlamingo-9B-vitl-mpt7b", "checkpoint.pt")
     model.load_state_dict(torch.load(checkpoint_path), strict=False)
     model.to(device)
+    
+    return model, image_processor, tokenizer
 
+def eval_model(model, image_processor, tokenizer, prompt, image_file):
     image = utils.load_image(image_file)
     vision_x = image_processor(image).unsqueeze(0).to(device)
     vision_x = vision_x.unsqueeze(1).unsqueeze(0)
@@ -58,13 +60,11 @@ def eval_model(prompt, image_file):
 
     return tokenizer.decode(generated_text[0])
 
+def predict_dataset(dataset_name, model_path, run, n_ic_samples=0):
+    # Load the model, tokenizer, and image_processor before looping over the dataset
+    model, image_processor, tokenizer = load_model()
 
-
-
-def predict_dataset(dataset_name, model_path, run, n_ic_samples):
-    
-
-    tasks, ds_file_path, image_dir_path, output_dir_path, output_file_path, config_file_path, split = utils.get_info(dataset_name = dataset_name, model_name = 'openflamingo', run = run)
+    tasks, ds_file_path, image_dir_path, output_dir_path, output_file_path, config_file_path, split = utils.get_info(dataset_name=dataset_name, model_name='openflamingo', run=run)
     dataset = pd.read_json(ds_file_path) 
     data_list = dataset['data'].tolist()
     
@@ -76,10 +76,27 @@ def predict_dataset(dataset_name, model_path, run, n_ic_samples):
         output_sample = {'text_input_id': test_sample['text_input_id']}
 
         for t in tasks:
-            prompt = prompts.zeroshot(test_sample, task)
-            image_file = os.path.join(image_dir_path, test_sample['image_id'])
-            output = eval_model(prompt, image_file)
+            prompt = prompts.zeroshot(test_sample, t)
 
+            # update prompt to insert the image token, so openflamingo knows where the image belongs to
+            prompt = "<image>" + prompt
+
+            print("prompt")
+            print(prompt)
+            image_file = os.path.join(image_dir_path, test_sample['image_id'])
+            output_raw = eval_model(model, image_processor, tokenizer, prompt, image_file)
+
+            print(f'raw output:')
+            print( output_raw)
+            
+            # openflamingo's output is constructed like this: prompt + self-generated text + <|endofchunk|> - extract only the self-generated text
+            if output_raw.startswith(prompt) and output_raw.endswith("<|endofchunk|>"):
+                output = output_raw[len(prompt):-14].strip()
+            else:
+                output = output_raw
+
+            print(f'output:')
+            print(output)
             output_name = 'output_' + t
             output_sample.update({output_name: output}) 
         pred.append(output_sample)
@@ -107,7 +124,7 @@ def predict_dataset(dataset_name, model_path, run, n_ic_samples):
 
 if __name__ == "__main__":
 
-    start_time= time.time() ########
+    start_time= time.time() 
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, required=True)
@@ -117,8 +134,8 @@ if __name__ == "__main__":
     predict_dataset(dataset_name = args.dataset, model_path = None, run = args.run)
 
     end_time= time.time()
-    run_time = end_time - start_time
-    print(f'runtime: {run_time}')
+    run_time = (end_time - start_time)/60
+    print(f'runtime (min): {run_time}')
 
 
 
