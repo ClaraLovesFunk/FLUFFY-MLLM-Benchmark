f7ca096622065e0981eca8b141e9fa99216a948b
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-10-13 10:21:59 +0000

    tidy idefics for dummy sample

--- models/idefics/inference.py
+++ models/idefics/inference.py
@@ -4,7 +4,6 @@ from transformers import IdeficsForVisionText2Text, AutoProcessor
 
 CACHE_DIR = '/home/users/cwicharz/project/Testing-Multimodal-LLMs/data/huggingface_cache'
 os.environ["TRANSFORMERS_CACHE"] = CACHE_DIR
-#os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb=1024'
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 torch.cuda.empty_cache()
@@ -15,9 +14,7 @@ def load_idefics_model(checkpoint):
     model = IdeficsForVisionText2Text.from_pretrained(
         checkpoint,
         torch_dtype=torch.bfloat16,
-        cache_dir=CACHE_DIR,
-        #device_map="auto",  # For efficient GPU utilization
-        #quantization_config=gptq_config
+        cache_dir=CACHE_DIR
     )
     model.to(device)
     
@@ -25,7 +22,7 @@ def load_idefics_model(checkpoint):
     
     return model, processor
 
-checkpoint = "HuggingFaceM4/idefics-9b-instruct" #"HuggingFaceM4/idefics-80b-instruct" 
+checkpoint = "HuggingFaceM4/idefics-9b-instruct" 
 model, processor = load_idefics_model(checkpoint)
 
 
@@ -46,21 +43,35 @@ prompts = [
     ],
 ]
 
-# --batched mode
-inputs = processor(prompts, add_end_of_utterance_token=False, return_tensors="pt").to(device)
-# --single sample mode
-# inputs = processor(prompts[0], return_tensors="pt").to(device)
 
-# Generation args
+
+
+inputs = processor(prompts, add_end_of_utterance_token=False, return_tensors="pt").to(device)
+
 exit_condition = processor.tokenizer("<end_of_utterance>", add_special_tokens=False).input_ids
 bad_words_ids = processor.tokenizer(["<image>", "<fake_token_around_image>"], add_special_tokens=False).input_ids
 
 generated_ids = model.generate(**inputs, eos_token_id=exit_condition, bad_words_ids=bad_words_ids, max_length=100)
-generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)
+output = processor.batch_decode(generated_ids, skip_special_tokens=True)
 
-print(generated_text)
-for i, t in enumerate(generated_text):
-    print(f"{i}:\n{t}\n")
+print(output)
 
 
 print('DOOOOOONNNE')
+
+
+
+
+
+
+
+# prompt_generic = prompts.zeroshot(test_sample=sample, task=t)
+# prompts = [
+#     [
+#         "User: " + prompt_generic,
+#         img_path,
+#         "<end_of_utterance>",
+
+#         "\nAssistant:",
+#     ],
+# ]
