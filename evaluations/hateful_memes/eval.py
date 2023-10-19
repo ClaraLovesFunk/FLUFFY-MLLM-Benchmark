d3ef242a250d1ba97e018a5720af602a11ffbeff
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-10-19 14:31:58 +0000

    further modularize hateful_memes eval

--- evaluations/hateful_memes/eval.py
+++ evaluations/hateful_memes/eval.py
@@ -1,56 +1,25 @@
-import json
-from sklearn import metrics
-import utils
+import evaluations.utils_eval as utils_eval
 
 VALID_ANS_VALUES = ['hateful', 'not hateful']
 TASK_NAME = "hate classification"
 POS_LABEL = "hateful"
+label_name = "classification_label"
+output_name = "output_hate classification"
 
-def load_json(filepath):
-    with open(filepath, 'r') as f:
-        return json.load(f)
 
-def compute_metrics(y_true, y_pred):
-    return {
-        'accuracy': metrics.accuracy_score(y_true, y_pred),
-        'precision': metrics.precision_score(y_true, y_pred, pos_label=POS_LABEL),
-        'recall': metrics.recall_score(y_true, y_pred, pos_label=POS_LABEL),
-        'f1': metrics.f1_score(y_true, y_pred, pos_label=POS_LABEL),                  
-    }
 
 def evaluate_hateful_memes(ds_text_file_path, experiment_output_file_path, model):
-    data_text = load_json(ds_text_file_path)
-    output = load_json(experiment_output_file_path)
 
-    id_to_label = {
-        item["text_input_id"]: item["classification_label"]
-        for item in data_text["data"] 
-        if "text_input_id" in item and "classification_label" in item
-    }
+    data_text = utils_eval.load_data(ds_text_file_path)
+    output = utils_eval.load_data(experiment_output_file_path)
+    labels = utils_eval.get_id_2_label_dict(data_text, label_name)
 
+    valid_ans_ratio, y_pred, y_true = utils_eval.get_clean_valid_preds_trues(output, output_name, VALID_ANS_VALUES, labels, model)
+    scores = utils_eval.compute_standard_metrics(y_true, y_pred, pos_label = POS_LABEL, average='binary')
+    examples = utils_eval.get_examples(output, output_name, labels)
 
-    y_true, y_pred = [], []
-    valid_count = 0
-
-    for item in output: 
-        
-        output_raw = item["output_hate classification"]
-        pred_value = utils.extract_answer(model, output_raw)
-
-        if pred_value in VALID_ANS_VALUES and item["text_input_id"] in id_to_label:
-            valid_count += 1
-            y_pred.append(pred_value)
-            y_true.append(id_to_label[item["text_input_id"]])
-
-    valid_ans_ratio = {TASK_NAME: valid_count / len(output) if output else 0}
-
-    scores = {TASK_NAME: compute_metrics(y_true, y_pred)}
-
-    examples = {
-        TASK_NAME: {
-            item["text_input_id"]: 1 if id_to_label[item["text_input_id"]] == item.get("output_hate classification") else 0
-            for item in output if "text_input_id" in item and item["text_input_id"] in id_to_label
-        }
-    }
+    valid_ans_ratio = {TASK_NAME: valid_ans_ratio}
+    scores = {TASK_NAME: scores}
+    examples = {TASK_NAME: examples}
 
     return scores, examples, valid_ans_ratio
