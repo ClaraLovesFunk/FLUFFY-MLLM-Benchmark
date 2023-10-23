58e3b6f54dab2d8902ae1af8110134b2b410d0a5
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-10-23 10:20:15 +0000

    ide: increase max_token form 100 to 250

--- models/idefics/inference.py
+++ models/idefics/inference.py
@@ -67,7 +67,7 @@ def gen_output(device, data_text, model, processor, image_dir_path, tasks):
             exit_condition = processor.tokenizer("<end_of_utterance>", add_special_tokens=False).input_ids
             bad_words_ids = processor.tokenizer(["<image>", "<fake_token_around_image>"], add_special_tokens=False).input_ids
 
-            generated_ids = model.generate(**inputs, eos_token_id=exit_condition, bad_words_ids=bad_words_ids, max_length=100)
+            generated_ids = model.generate(**inputs, eos_token_id=exit_condition, bad_words_ids=bad_words_ids, max_length=250)
             output = processor.batch_decode(generated_ids, skip_special_tokens=True)
             output_name = "output_" + task
             output_sample.update({output_name: output[0]})
