f9827560bfe0a73f266e88ba505c7f4ccb9a529a
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-10-24 14:33:09 +0000

    only use imgs from local machine

--- models/otter/inference.py
+++ models/otter/inference.py
@@ -1,11 +1,8 @@
 from __future__ import annotations
 
-import mimetypes
 import os
 from io import BytesIO
 from typing import Union
-#import cv2
-import requests
 import torch
 import transformers
 from PIL import Image
@@ -20,31 +17,18 @@ sys.path.append("models/otter")
 from otter_ai import OtterForConditionalGeneration
 
 # Disable warnings
-requests.packages.urllib3.disable_warnings()
+#requests.packages.urllib3.disable_warnings()
 
 # ------------------- Utility Functions -------------------
 
-def get_content_type(file_path):
-    content_type, _ = mimetypes.guess_type(file_path)
-    return content_type
+# def get_content_type(file_path):
+#     content_type, _ = mimetypes.guess_type(file_path)
+#     return content_type
 
 # ------------------- Image Handling Functions -------------------
 
-def get_image(url: str) -> Union[Image.Image, list]:
-    if not url.strip():  # Blank input, return a blank Image
-        return Image.new("RGB", (224, 224))  # Assuming 224x224 is the default size for the model. Adjust if needed.
-    elif "://" not in url:  # Local file
-        content_type = get_content_type(url)
-    else:  # Remote URL
-        content_type = requests.head(url, stream=True, verify=False).headers.get("Content-Type")
-
-    if "image" in content_type:
-        if "://" not in url:  # Local file
-            return Image.open(url)
-        else:  # Remote URL
-            return Image.open(requests.get(url, stream=True, verify=False).raw)
-    else:
-        raise ValueError("Invalid content type. Expected image.")
+def get_image(file_path: str) -> Image.Image:
+    return Image.open(file_path)
 
 # ------------------- OTTER Prompt and Response Functions -------------------
 
