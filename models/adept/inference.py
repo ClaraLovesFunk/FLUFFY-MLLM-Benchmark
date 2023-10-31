b7f4c842caf20ecfc1cdc1c34de52264664fea88
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-10-31 10:27:09 +0000

    adept works on sample

--- models/adept/inference.py
+++ models/adept/inference.py
@@ -1,21 +1,25 @@
 from transformers import FuyuProcessor, FuyuForCausalLM
 from PIL import Image
+import torch
 
-# load model and processor
+device = torch.device("cpu") # "cuda" if torch.cuda.is_available() else "cpu")
+
+
 model_id = "adept/fuyu-8b"
+model = FuyuForCausalLM.from_pretrained(model_id)
+model.to(device)
 processor = FuyuProcessor.from_pretrained(model_id)
-model = FuyuForCausalLM.from_pretrained(model_id, device_map="cuda:0")
 
 # prepare inputs for the model
 text_prompt = "Generate a coco-style caption.\n"
-image_path = "bus.png"  # https://huggingface.co/adept-hf-collab/fuyu-8b/blob/main/bus.png
+image_path = "/home/users/cwicharz/project/Testing-Multimodal-LLMs/datasets/mami/images/all/1.jpg"  
 image = Image.open(image_path)
 
 inputs = processor(text=text_prompt, images=image, return_tensors="pt")
 for k, v in inputs.items():
-    inputs[k] = v.to("cuda:0")
+    inputs[k] = v.to(device)
 
 # autoregressively generate text
-generation_output = model.generate(**inputs, max_new_tokens=7)
+generation_output = model.generate(**inputs, max_new_tokens=7) 
 generation_text = processor.batch_decode(generation_output[:, -7:], skip_special_tokens=True)
-assert generation_text == ['A bus parked on the side of a road.']
+print(generation_text)
