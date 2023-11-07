7f35e36e7549fda7662f701c79ad9e7ef43cf13a
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-11-07 15:01:32 +0000

    aokvqa corrected scores format

--- evaluations/okvqa/eval.py
+++ evaluations/okvqa/eval.py
@@ -44,12 +44,10 @@ def acc_okvqa(annFile, quesFile, resFile, resFile_original, transform_output_4_o
 
 def evaluate_okvqa(ds_text_file_path, experiment_output_file_path, model):
 
-    print(experiment_output_file_path)
     ds_dir = os.path.dirname(ds_text_file_path)
     ds_text_annotations_file_path = os.path.join(ds_dir, 'ds_original_labels.json')
     ds_text_questions_file_path = os.path.join(ds_dir, 'ds_original.json')
     experiment_output_okvqa_format_file_path = os.path.join(ds_dir, 'output_okvqa_format.json')
-    #experiment_scores_file_path = experiment_output_file_path
 
     scores = {}
     examples = {}
@@ -60,26 +58,16 @@ def evaluate_okvqa(ds_text_file_path, experiment_output_file_path, model):
                     experiment_output_file_path, 
                     transform_output_4_okvqa)
 
-    scores['direct answer'] = {'accuracy': acc} 
+    scores = {'accuracy': acc} 
 
-    # delete output file in okvqa format, after it has been used for evalation
-    os.remove(experiment_output_okvqa_format_file_path)
+    os.remove(experiment_output_okvqa_format_file_path) # delete output file in okvqa format, after it has been used for evalation
 
-    examples['direct answer'] = 'test' ##################################################################### IDENTIFY EXAMPLES!
+    data_text = utils_eval.load_data(ds_text_file_path)
+    output = utils_eval.load_data(experiment_output_file_path)
+    labels = utils_eval.get_id_2_label_dict(data_text, label_name, dataset_name) #
 
+    examples = utils_eval.get_examples(output, output_name, labels)
 
-
-
-
-    # data_text = utils_eval.load_data(ds_text_file_path)
-    # output = utils_eval.load_data(experiment_output_file_path)
-    # labels = utils_eval.get_id_2_label_dict(data_text, label_name, dataset_name)
-
-    # #valid_ans_ratio, y_pred, y_true = utils_eval.get_clean_valid_preds_trues(output, output_name, VALID_ANS_VALUES, labels, model, dataset_name, data_text)
-    # scores = utils_eval.compute_standard_metrics(y_true, y_pred, pos_label = POS_LABEL, average='binary', flag_only_acc = False)
-    # examples = utils_eval.get_examples(output, output_name, labels)
-
-    #valid_ans_ratio = {TASK_NAME: valid_ans_ratio}
     scores = {TASK_NAME: scores}
     examples = {TASK_NAME: examples}
 
