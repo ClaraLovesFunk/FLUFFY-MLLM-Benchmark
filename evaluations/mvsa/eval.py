695dd7461edd4274ef8c88a1ac17a85b532095ea
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-11-23 17:15:54 +0000

    add mvsa

--- evaluations/mvsa/eval.py
+++ evaluations/mvsa/eval.py
@@ -1,4 +1,5 @@
 import evaluations.utils_eval as utils_eval
+import utils
 
 VALID_ANS_VALUES = ['positive', 'negative', 'neutral']
 TASK_NAME = "sentiment analysis"
@@ -9,19 +10,25 @@ dataset_name = "mvsa"
 
 
 
-def evaluate_mvsa(ds_text_file_path, experiment_output_file_path, model, mode):
+def evaluate_mvsa(CONFIG_PATH, dataset_name, model_name, mode, run):
 
-    data_text = utils_eval.load_data(ds_text_file_path)
-    output = utils_eval.load_data(experiment_output_file_path)
-    labels = utils_eval.get_id_2_label_dict(data_text, label_name, dataset_name)
+    # preprocess output & get valid answer ratio 
+    y_pred_dict, y_true_dict, label2_y_pred_dict, valid_ans_ratio_dict = utils_eval.pipeline_preprocess(
+         CONFIG_PATH, VALID_ANS_VALUES, dataset_name, model_name, run, mode)
+    
+    # do the official evaluation, but with output data transformed according to evaluation modus
+    scores_dict = {}
+    examples_dict = {}
+    
+    DatasetInfo = utils.DatasetInfo(dataset_name)
+    tasks = DatasetInfo.get_tasks()
+    for task in tasks:
+        y_pred = y_pred_dict[task]
+        y_true = y_true_dict[task]
 
-    valid_ans_ratio, y_pred, y_true = utils_eval.get_clean_valid_preds_trues(output, output_name, VALID_ANS_VALUES, labels, model, dataset_name, data_text, mode)
-    scores = utils_eval.compute_standard_metrics(y_true, y_pred, pos_label = POS_LABEL, average='weighted', zero_division=0, flag_only_acc = False)
+        #print(label2_y_pred_dict[task]["1.jpg"])
 
-    examples = utils_eval.get_examples(dataset_name, output, output_name, labels)
+        scores = utils_eval.compute_standard_metrics(y_true, y_pred, pos_label = POS_LABEL, average='weighted', zero_division=0, flag_only_acc = False)
+        scores_dict[task] = scores
 
-    valid_ans_ratio = {TASK_NAME: valid_ans_ratio}
-    scores = {TASK_NAME: scores}
-    examples = {TASK_NAME: examples}
-
-    return scores, examples, valid_ans_ratio
+    return scores_dict, examples_dict, valid_ans_ratio_dict
