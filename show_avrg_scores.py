bbd8fdfbc96a5f5773d50fea2d9eb315e77c5eaa
ClaraLovesFunk <clara.wicharz@gmail.com>
2023-11-26 09:37:31 +0000

    calc avrg works outside of run_eval.py

--- show_avrg_scores.py
+++ show_avrg_scores.py
@@ -2,19 +2,19 @@ import json
 import os
 import evaluations.utils_eval as utils_eval
 import utils
-
-
+import math  # Importing math to check for NaN
 
 CONFIG_PATH = 'config.json'
 
 config = utils_eval.load_data(CONFIG_PATH)
-model_names = ['idefics'] #config['model_names']
+model_names = ['idefics']  # config['model_names']
 dataset_names = config['dataset_names']
 mode_names = ['soft', 'hard']
 
 
 def average(values):
-    return sum(values) / len(values) if values else 0
+    valid_values = [v for v in values if not math.isnan(v)]  # Filter out NaN values
+    return sum(valid_values) / len(valid_values) if valid_values else 0
 
 for mode_name in mode_names:
     print(f"Mode: {mode_name}")
@@ -25,8 +25,16 @@ for mode_name in mode_names:
             file_data = utils_eval.load_data(scores_file_path)
             DatasetInfo = utils.DatasetInfo(dataset_name)
             tasks = DatasetInfo.get_tasks()
-            dataset_scores = {task: file_data[task]['accuracy'] for task in tasks}
+            dataset_scores = {}
+            for task in tasks:
+                task_score = file_data[task]['accuracy']
+                dataset_scores[task] = task_score if not math.isnan(task_score) else 0  # Handle NaN here if needed
+            #print(dataset_scores)
             dataset_scores_average = average(dataset_scores.values())
+            #print(dataset_scores_average)
             model_scores[dataset_name] = dataset_scores_average
-        model_scores_average = average(model_scores.values())
+        model_scores_average = {"average accuracy": average(model_scores.values())}
         print(f"{model} Average Accuracy: {model_scores_average}")
+
+        scores_average_file_path = f"experiments/{model}/scores_average_{mode_name}.json" # needs to be edited when running more runs
+        utils_eval.save_data(scores_average_file_path, model_scores_average)
