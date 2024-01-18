61f0935f700e88d14ebf2056a536dc1ba11a852d
ClaraLovesFunk <clara.wicharz@gmail.com>
2024-01-18 18:48:44 +0100

    style prompts & remove unused code

--- prompts.py
+++ prompts.py
@@ -1,8 +1,3 @@
-import pandas as pd
-import random
-
-
-
 def zeroshot(test_sample, task):
     question_formal = 'Questions: '
     choices_formal = 'Choices: '
@@ -49,48 +44,3 @@ def zeroshot(test_sample, task):
         prompt += f'\n{hypothesis_text_formal}{text_input}\n{entailment_label}'
 
     return prompt
-
-
-
-'''
-
-ds_file_path = "datasets/hateful_memes/ds_benchmark_fulllabel.json"
-dataset = pd.read_json(ds_file_path) 
-data_list = dataset['data'].tolist()'''
-
-
-
-def fewshot_openflamingo(samples_IC, sample_query):
-    
-    prompt = ""
-
-    # add in-context samples
-    for s in samples_IC:
-        print(str(s['classification_label']))
-        prompt += "<image>This meme is " + str(s['classification_label']) + ".<|endofchunk|>"  
-
-    # add query sample
-    prompt += "<image>This meme is" + str(sample_query['classification_label'])  
-
-    return prompt
-
-
-def ICsamples_random(sample_query, data_list, n):
-
-    # Remove the sample_query from the data_list if present
-    data_list = [sample for sample in data_list if sample != sample_query]
-    
-    # Randomly select n samples
-    random_samples = random.sample(data_list, n)
-
-    return random_samples
-
-
-'''
-sample_query = data_list[2]
-n = 3
-
-random_samples = ICsamples_random(sample_query, data_list, n)
-prompt = fewshot_openflamingo(samples_IC = random_samples, sample_query = sample_query)
-print(f'prompt: {prompt}')
-'''
