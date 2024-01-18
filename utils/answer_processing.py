231298c9edc3b7c9a7f2c35559f5acc52f947f99
ClaraLovesFunk <clara.wicharz@gmail.com>
2024-01-18 14:46:18 +0100

    mod utils_eval: move functions related to output processing to answer_processing.py; move functions related to transforming output to data_transformation.py

--- utils/answer_processing.py
+++ utils/answer_processing.py
@@ -198,3 +198,50 @@ def get_clean_valid_preds_trues(output, output_name, VALID_ANS_VALUES, labels, m
     #print(f'valid_ans_ratio: {valid_ans_ratio}')
 
     return valid_ans_ratio, y_pred, y_true, y_pred_dict, y_true_dict
+
+
+def pipeline_preprocess(CONFIG_PATH, VALID_ANS_VALUES, dataset_name, model_name, run, mode):
+    '''
+    preprocess output depending on the model, dataset and evaluation modus
+    save preprocessed output in new file
+    return predictions once as array [prediction, prediction, ...] for further evaluation with scikitlearn
+    return predictions as dictionary {text_input_id: prediction} for other shannanagans
+    '''
+    dataset_benchmark_path = get_paths(CONFIG_PATH, dataset_name, model_name, run, mode, value_of_interest = 'dataset_benchmark_path')
+    output_original_path = get_paths(CONFIG_PATH, dataset_name, model_name, run, mode, value_of_interest = 'output_original_path')
+    
+    dataset_benchmark = load_data(dataset_benchmark_path)["data"]
+    output_original = load_data(output_original_path)
+
+    valid_ans_ratio_dict = {} 
+    label2_y_pred_dict = {}
+    y_pred_dict = {}
+    y_true_dict = {}
+
+    DatasetInfo = utils.DatasetInfo(dataset_name)
+    tasks = DatasetInfo.get_tasks()
+
+    for task in tasks:
+        label_name = utils.get_task2label_name(task)
+        labels = get_id_2_label_dict(dataset_benchmark, label_name, dataset_name) 
+        
+        valid_ans_ratio, y_pred, y_true, y_pred_dict, y_true_dict = get_clean_valid_preds_trues(
+            output = output_original, 
+            output_name = "output_"+ task, 
+            VALID_ANS_VALUES = VALID_ANS_VALUES, 
+            labels = labels, 
+            model_name = model_name, 
+            dataset_name = dataset_name, 
+            data_text = dataset_benchmark, 
+            mode = mode, 
+            task = task)
+        
+        
+        label2_y_pred_dict[task] = y_pred_dict
+        valid_ans_ratio_dict[task] = valid_ans_ratio
+        y_pred_dict[task] = y_pred
+        y_true_dict[task] = y_true
+    
+    make_output_aux_eval(CONFIG_PATH, dataset_name, model_name, run, tasks, mode, label2_y_pred_dict)
+
+    return  y_pred_dict, y_true_dict, label2_y_pred_dict, valid_ans_ratio_dict
--- utils/data_transformation.py
+++ utils/data_transformation.py
@@ -1,3 +1,6 @@
+import os
+from .file_operations import load_data
+
 def get_id_2_label_dict(data_text, label_name, dataset_name):
     '''
     {text_input_id: label}
@@ -9,3 +12,29 @@ def get_id_2_label_dict(data_text, label_name, dataset_name):
         labels[text_input_id] = label_value
 
     return labels
+
+
+def make_output_aux_eval(CONFIG_PATH, dataset_name, model_name, run, tasks, mode, y_pred_dict_all_tasks):
+    """
+    - load the original outputfile
+    - replace all predictions with our new cleaned predictions (cleaned, based on model requiriements, dataset and evaluation modus) we got
+        in pipeline_preprocess()
+    - save the transformed output with a new name
+    """
+    output_original_path = get_paths(CONFIG_PATH, dataset_name, model_name, run, mode, value_of_interest = 'output_original_path')
+    output_original = load_data(output_original_path)
+
+    for task in tasks:
+        y_pred_dict = y_pred_dict_all_tasks[task]
+        for item in output_original:
+            text_input_id = item.get("text_input_id")
+            y_pred_value = y_pred_dict.get(text_input_id)
+            if y_pred_value:
+                item["output_" + task] = y_pred_value
+    output_transformed = output_original # you shall now be called output_transformed
+
+    output_transformed_path = get_paths(CONFIG_PATH, dataset_name, model_name, run, mode, value_of_interest = 'output_transformed_path')
+    directory = os.path.dirname(output_transformed_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    save_data(output_transformed_path, output_transformed)
